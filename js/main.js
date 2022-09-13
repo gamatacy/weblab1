7beeaf2f551e6980f5d4af9c5657410f5fb247c6
gamatacy <voroninivan003@gmail.com>
2022-09-13 18:14:25 +0300

    ready

--- js/main.js
+++ js/main.js
@@ -1,22 +1,77 @@
 $(document).ready(function () {
+    let attempts = 0;
 
-    function checkY(y) {
-        return !(y > 5 || y < -5);
+    $(".check-button").click(function () {
+            let x = $("#x-value").val();
+            let y = $(".y-text-area").val();
+            let r;
+            $(".r-checkbox").each(function () {
+                if ($(this).is(":checked")) {
+                    r = $(this).val();
+                    return false
+                }
+            })
+
+            if (!checkCoordsValid(x, y, r)) {
+                invalidValue()
+                return false
+            }
+
+            $.get({
+                url: "./php/main.php",
+                data: {
+                    x: x,
+                    y: y,
+                    r: r,
+                    time: new Date()
+                },
+                dataType: 'JSON'
+            }).done(function (data) {
+                createResultRow(data);
+            })
+        }
+    )
+
+    function checkCoordsValid(x, y, r) {
+        return !!(checkX(x) && checkY(y) && checkX(r));
     }
 
-    $("#check-button").click(
-        $.ajax({
-            type: "GET",
-            url: "./php/main.php",
-            data: {
-                x: 1,
-                y: 13,
-                r: 14,
-                time: new Date()
+    function checkY(y) {
+        return (y <= 5 && y >= -5)
+    }
+
+    function checkX(x) {
+        return !(x === undefined)
+    }
+
+    function invalidValue() {
+        $(".check-button").html("Invalid value")
+        $(".check-button").toggleClass("check-button-active")
+        setTimeout(() => {
+                $(".check-button").html("check")
+                $(".check-button").removeClass("check-button-active")
             },
-            success: function (data) {
-                alert(data);
-            }
-        })
-    )
+            1000)
+
+    }
+
+    function createResultRow(data) {
+
+        let time = new Date(data.time);
+        let exectime = (data.execution).toString().slice(0,19);
+        $(".results-table").append(
+            `<tr class='result-row'>
+             <th class='result-cell'>${data.result}</th>
+             <th class='result-cell'>${data.x}</th>
+             <th class='result-cell'>${data.y}</th>
+             <th class='result-cell'>${data.r}</th>
+             <th class='result-cell'>${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}</td>
+             <th class='result-cell'>${exectime}</th>
+             <th class="result-cell">${attempts}</th>
+             </tr>
+            `
+        )
+        attempts++;
+    }
+
 });
--- php/main.php
+++ php/main.php
@@ -1,11 +1,36 @@
 <?php
 
+$start_time = microtime(true);
 $x = $_GET['x'];
 $y = $_GET['y'];
 $r = $_GET['r'];
 $time = $_GET['time'];
+$result = (checkCircle($x, $y, $r) or checkSquare($x, $y, $r) or checkTriangle($x, $y, $r));
+$end_time = microtime(true) - $start_time;
 
-echo $x;
-echo $y;
-echo $r;
-echo $time;
+$response = array(
+    'x' => $x,
+    'y' => $y,
+    'r' => $r,
+    'time' => $time,
+    'result' => $result,
+    'execution' => $end_time
+);
+
+echo json_encode($response);
+
+function checkCircle($x, $y, $r)
+{
+    return (pow($x, 2) + pow($y, 2) <= pow($r, 2) and ($y >= 0 and $x <= 0));
+
+}
+
+function checkSquare($x, $y, $r)
+{
+    return (($x <= $r / 2) and ($y <= $r) and ($x >= 0 and $y >= 0));
+}
+
+function checkTriangle($x, $y, $r)
+{
+    return ($y >= -((-$x + $r) / 2)) and ($y >= (-$r / 2) and $y <= 0) and ($x >= 0 and $x <= $r);
+}
